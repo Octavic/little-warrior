8c239f6b194a4690a20367db2546030789ee9d98
Octavic <yifeixu@live.com>
2018-10-23 17:07:21 -0700

    Add Pattern and tests

---
+++ LittleWarrior/Assets/Scripts/Equipment/Pattern.cs
@@ -0,0 +1,48 @@
+﻿namespace Assets.Scripts.Equipment
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Describes all of the possible slices
+    /// The letters maps to locations as following
+    /// --------------
+    /// |   A | E    |
+    /// | B   |    F |
+    /// --------------
+    /// | C   |    G |
+    /// |   E | H    |
+    /// --------------
+    /// </summary>
+    public enum Pattern
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+    }
+
+    /// <summary>
+    /// Extends the Pattern enum
+    /// </summary>
+    public static class PatternExtensions
+    {
+        /// <summary>
+        /// Gets the opposite "slice"
+        /// </summary>
+        /// <param name="p">Pattern base class</param>
+        /// <returns>The opposite slice</returns>
+        public static Pattern GetOpposite(this Pattern p)
+        {
+            var asInt = (int)p;
+            var result = (asInt + 4) % 8;
+            return (Pattern)result;
+        }
+    }
+}
